7436f74ca5043b70f59bb09c896e4de596e5cc79
Andreas Stührk <a.stuehrk@gini.net>
2014-11-01 23:26:55 +0100

    Replace hardcoded list of docker hosts by config property.

--- README.md
+++ README.md
@@ -10,4 +10,7 @@ Like synapse, but for breakerbox.
 2. Add the following line to your breakerbox's properties file:
    `InstanceDiscovery.impl=de.hammerhartes.andy.breakerbox.DockerDiscovery`
 
+3. Add your docker hosts to your breakerbox's properties file:
+   `docker.hosts=http://localhost:2375`
+
 3. Add `build/libs/docker_discovery.jar` to breakerbox's classpath. 
--- src/main/java/de/hammerhartes/andy/breakerbox/DockerDiscovery.java
+++ src/main/java/de/hammerhartes/andy/breakerbox/DockerDiscovery.java
@@ -2,6 +2,7 @@ package de.hammerhartes.andy.breakerbox;
 
 import com.google.common.collect.ImmutableList;
 
+import com.netflix.config.ConfigurationManager;
 import com.netflix.turbine.discovery.Instance;
 import com.netflix.turbine.discovery.InstanceDiscovery;
 import com.sun.jersey.api.client.Client;
@@ -10,31 +11,35 @@ import de.hammerhartes.andy.breakerbox.command.GetContainersCommand;
 import de.hammerhartes.andy.breakerbox.model.Container;
 import de.hammerhartes.andy.breakerbox.model.Port;
 
+import org.apache.commons.configuration.AbstractConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.net.URI;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static java.lang.String.format;
+import static java.util.stream.Collectors.toList;
 
 public class DockerDiscovery implements InstanceDiscovery {
 
     private static final Logger LOG = LoggerFactory.getLogger(DockerDiscovery.class);
     private static final String CLUSTER_NAME = "prod";
-    private static final Iterable<URI> DOCKER_HOSTS = ImmutableList.of(
-            URI.create("http://localhost:2375"));
+    private static final String DOCKER_HOSTS_KEY = "docker.hosts";
     private final Client client;
+    private final List<URI> dockerHosts;
 
     public DockerDiscovery() {
         client = Client.create();
+        dockerHosts = getDockerHosts();
     }
 
     @Override
     public Collection<Instance> getInstanceList() throws Exception {
         final ImmutableList.Builder<Instance> instances = ImmutableList.builder();
-        for (final URI dockerHost : DOCKER_HOSTS) {
+        for (final URI dockerHost : dockerHosts) {
             final List<Container> containers = new GetContainersCommand(client, dockerHost).execute();
             LOG.info("Found {} containers", containers.size());
             containers.stream()
@@ -47,5 +52,12 @@ public class DockerDiscovery implements InstanceDiscovery {
                     });
         }
         return instances.build();
+    }
+
+    private static List<URI> getDockerHosts() {
+        final AbstractConfiguration config = ConfigurationManager.getConfigInstance();
+        return Stream.of(config.getStringArray(DOCKER_HOSTS_KEY))
+                .map(URI::create)
+                .collect(toList());
     }
 }
